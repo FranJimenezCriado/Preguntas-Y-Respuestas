4e90d8f5539361154242d6f1e0e52ba4dd552cf8
Fran <franciscojc1995@gmail.com>
2024-01-18 23:32:39 +0100

    Script acabado

--- js/script.js
+++ js/script.js
@@ -21,29 +21,30 @@ function preguntasYRespuestas() {
 
                     const contenedor = document.getElementById("contenedor");
 
-                    const contenedor2 = document.createElement("div");
+                    const contenedorFinal = document.createElement("div");
 
-                    contenedor2.className = "acabar";
+                    contenedorFinal.className = "acabar";
 
-                    contenedor2.innerHTML =
+                    contenedorFinal.innerHTML =
                 `   
                     <div><p>Has terminado, tu puntuacion es ${puntuacion}</p></div>
                 `
-                contenedor.appendChild(contenedor2);
+                contenedor.appendChild(contenedorFinal);
 
                 }
 
-                // Esta variable posiciona al dom en el div con id = "contenedor" de nuestro html
+
                 const contenedor = document.getElementById("contenedor");
-                // Esta variable crea un div nuevo para cada juego que vamos a mostrar usando el dom
+
                 const contenedor2 = document.createElement("div");
-                // Hacemos que todos los div (que seran las tarjetas de los juegos) tengan la misma clase: "juego"
-                contenedor2.className = "preguntayrespuesta";
-                // Ahora la variable que ya tiene el nuevo div la llenamos con el contenido deseado
+
+                if(c < data.length) {
+                    contenedor2.className = "preguntayrespuesta";
+
                 contenedor2.innerHTML =
                 `   
                     <div class="pregunta">
-                    <h2>${data[c].question}</h2>
+                    <h2>Pregunta ${c + 1}: ${data[c].question}</h2>
                     </div>
                     <button id="respuesta1">
                     ${data[c].answers[0]}
@@ -58,11 +59,9 @@ function preguntasYRespuestas() {
                     ${data[c].answers[3]}
                     </button>
                 `
-                // Finalmente metemos el contenido deseado dentro del div con id="contenedor" que ya obtuvimos usando la variable contenedor
+
                 contenedor.appendChild(contenedor2);
-
-
-
+                }
 
                     const boton1 = document.getElementById("respuesta1");
     
@@ -72,179 +71,153 @@ function preguntasYRespuestas() {
         
                     const boton4 = document.getElementById("respuesta4");
         
-                    const respuesta1 = data[c].answers[0];
+                    let respuesta1;
         
-                    const respuesta2 = data[c].answers[1];
+                    let respuesta2;
         
-                    const respuesta3 = data[c].answers[2];
+                    let respuesta3;
         
-                    const respuesta4 = data[c].answers[3];
+                    let respuesta4;
 
-                    console.log(respuesta1);
+                    if (c < data.length) {
 
-                    console.log(respuesta2);
+                        respuesta1 = data[c].answers[0];
 
-                    console.log(respuesta3);
+                        respuesta2 = data[c].answers[1];
 
-                    console.log(respuesta4);
+                        respuesta3 = data[c].answers[2];
+
+                        respuesta4 = data[c].answers[3];
+
+                    }
     
-                    boton1.addEventListener('click',(event) => {
+                    if (c < data.length) {
+                        boton1.addEventListener('click',(event) => {
     
-                        while(c < data.length) {
-        
-        
-                            if(respuesta1 === data[c].correct) {
-        
-                                c++;
-        
-                                console.log(c);
-
-                                contenedor2.parentNode.removeChild(contenedor2);
-
-                                puntuacion++;
-
-                                console.log("Tienes estos puntos:", puntuacion);
+                            while(c < data.length) {
+            
+            
+                                if(respuesta1 === data[c].correct) {
+            
+                                    c++;
     
-                                mostrar();
-
-                                break;
-        
-                            } else {
-
-                                c++;
-
-                                console.log(c);
-
-                                contenedor2.parentNode.removeChild(contenedor2);
-
-                                console.log("No has ganado puntos");
-
-                                mostrar();
-
-                                break;
-
-                            }
-                        };
-        
-                    });
-        
-                    boton2.addEventListener('click',(event) => {
-        
-                        while(c < data.length) {
-        
-                            if(respuesta2 === data[c].correct) {
-        
-                                c++;
-        
-                                console.log(c);
-
-                                contenedor2.parentNode.removeChild(contenedor2);
-
-                                puntuacion++;
-
-                                console.log("Tienes estos puntos:", puntuacion);
+                                    contenedor2.parentNode.removeChild(contenedor2);
     
-                                mostrar();
-
-                                break;
-
-                            } else {
-
-                                c++;
-
-                                console.log(c);
-
-                                contenedor2.parentNode.removeChild(contenedor2);
-
-                                console.log("No has ganado puntos");
-
-                                mostrar();
-                                
-                                break;
-
-                            }
-                        };
+                                    puntuacion++;
         
-                    });
-        
-                    boton3.addEventListener('click',(event) => {
-        
-                        while(c < data.length) {
-        
-                            if(respuesta3 === data[c].correct) {
-        
-                                c++;
-        
-                                console.log(c);
-
-                                contenedor2.parentNode.removeChild(contenedor2);
-
-                                puntuacion++;
-
-                                console.log("Tienes estos puntos:", puntuacion);
+                                    mostrar();
     
-                                mostrar();
-
-                                break;
-        
-                            } else {
-                                
-                                c++;
-
-                                console.log(c);
-
-                                contenedor2.parentNode.removeChild(contenedor2);
-
-                                console.log("No has ganado puntos");
-
-                                mostrar();
-                                
-                                break;
-
-                            }
-                        };
-        
-                    });
-        
-                    boton4.addEventListener('click',(event) => {
-        
-        
-                        while(c < data.length) {
-        
-                            if(respuesta4 === data[c].correct) {
-        
-                                c++;
-        
-                                console.log(c);
-
-                                contenedor2.parentNode.removeChild(contenedor2);
-
-                                puntuacion++;
-
-                                console.log("Tienes estos puntos:", puntuacion);
+                                    break;
+            
+                                } else {
     
-                                mostrar();
-
-                                break;
+                                    c++;
+    
+                                    contenedor2.parentNode.removeChild(contenedor2);
+    
+                                    mostrar();
+    
+                                    break;
+    
+                                }
+                            };
+            
+                        });
+            
+                        boton2.addEventListener('click',(event) => {
+            
+                            while(c < data.length) {
+            
+                                if(respuesta2 === data[c].correct) {
+            
+                                    c++;
+    
+                                    contenedor2.parentNode.removeChild(contenedor2);
+    
+                                    puntuacion++;
         
-                            } else {
-                                
-                                c++;
-
-                                console.log(c);
-
-                                contenedor2.parentNode.removeChild(contenedor2);
-
-                                console.log("No has ganado puntos");
-
-                                mostrar();
-                                
-                                break;
-
-                            }
-                        };
-
-                    });
+                                    mostrar();
+    
+                                    break;
+    
+                                } else {
+    
+                                    c++;
+    
+                                    contenedor2.parentNode.removeChild(contenedor2);
+    
+                                    mostrar();
+                                    
+                                    break;
+    
+                                }
+                            };
+            
+                        });
+            
+                        boton3.addEventListener('click',(event) => {
+            
+                            while(c < data.length) {
+            
+                                if(respuesta3 === data[c].correct) {
+            
+                                    c++;
+    
+                                    contenedor2.parentNode.removeChild(contenedor2);
+    
+                                    puntuacion++;
+        
+                                    mostrar();
+    
+                                    break;
+            
+                                } else {
+                                    
+                                    c++;
+    
+                                    contenedor2.parentNode.removeChild(contenedor2);
+    
+                                    mostrar();
+                                    
+                                    break;
+    
+                                }
+                            };
+            
+                        });
+            
+                        boton4.addEventListener('click',(event) => {
+            
+            
+                            while(c < data.length) {
+            
+                                if(respuesta4 === data[c].correct) {
+            
+                                    c++;
+    
+                                    contenedor2.parentNode.removeChild(contenedor2);
+    
+                                    puntuacion++;
+        
+                                    mostrar();
+    
+                                    break;
+            
+                                } else {
+                                    
+                                    c++;
+    
+                                    contenedor2.parentNode.removeChild(contenedor2);
+    
+                                    mostrar();
+                                    
+                                    break;
+    
+                                }
+                            };
+    
+                        });
+                    }
 
             };
 
